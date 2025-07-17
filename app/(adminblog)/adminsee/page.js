a1d5f011a72aaed92a781c393cc5e6e770a42fc3
jeevananthamS15 <jeevanantham.s2023ai-ds@sece.ac.in>
2025-07-17 23:37:06 +0530

    admin description.10

--- app/(adminblog)/adminsee/page.js
+++ app/(adminblog)/adminsee/page.js
@@ -2,9 +2,12 @@
 export const dynamic = "force-dynamic";
 
 import { Suspense } from "react";
-import loadDynamic from "next/dynamic"; 
+import dynamicImport from "next/dynamic";
 
-const Blogs = loadDynamic(() => import("@/Blogcomponent/Blogs"), { ssr: false });
+
+const Blogs = dynamicImport(() => import("@/Blogcomponent/Blogs"), {
+  ssr: false,
+});
 
 const AdminSee = () => {
   return (
